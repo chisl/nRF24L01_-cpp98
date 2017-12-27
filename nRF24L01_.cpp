bfffe1b82c448400f266b4d29a857d879538ec61
chisl <info@chisl.io>
2017-12-27 18:31:05 +0100

    Initial commit

---
+++ nRF24L01_.cpp
@@ -0,0 +1,13 @@
+/*
+ * name:        nRF24L01+
+ * description: Single Chip 2.4GHz Transceiver
+ * manuf:       Nordic Semiconductor
+ * version:     0.1
+ * url:         https://www.nordicsemi.com/eng/content/download/2726/34069/file/nRF24L01P_Product_Specification_1_0.pdf
+ * date:        2017-12-19
+ * author       https://chisl.io/
+ * file:        nRF24L01_.cpp
+ */
+
+#include "nRF24L01_.hpp"
+
